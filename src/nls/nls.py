134b127922fef5dd82af7cb2192f521e135d8156
Salies <serezane@protonmail.com>
2022-07-04 01:10:30 -0300

    nls melhorado

--- src/nls/nls.py
+++ src/nls/nls.py
@@ -6,98 +6,71 @@
 # Implementações baseadas nos pseudoalgoritmos do VMLS.
 
 import numpy as np
-import numpy.linalg as npl
 import sympy
 
-# Funções para resolver as equações lineares,
-# lidando com "inversões" de matrizes 1x1.
-def safe_solve(fx, Dfx):
-    if(isinstance(Dfx, float)):
-        return np.divide(fx, Dfx)
-    return np.linalg.inv(Dfx) @ fx
-
-def safe_lstsq(A, b):
-    if(isinstance(A, float)):
+# Suporte para manipulação de escalares
+def lstsq(A, b):
+    if(np.isscalar(A)):
         return np.divide(b, A)
-    return np.linalg.lstsq(A, b, rcond = None)[0]
+    return np.linalg.inv((A.T @ A)) @ A.T @ b
 
-def safe_matrix(A):
-    if(isinstance(A, float)):
-        return np.vstack([A])
-    return A
+def solve(A, b):
+    if(np.isscalar(A)):
+        return np.divide(b, A)
+    return np.linalg.inv(A) @ b
 
-def gauss_newton(f, Df, x1, k_max = 10, err = 1e-6):
-    # Define o x a ser calculado, valor inicial em x1
+# Para lm
+# Stopping criteria - Small optimality condition residual
+def lm_op_res(A, b):
+    if(np.isscalar(A)):
+        return 2 * A * b
+    return 2 * A.T @ b
+
+def lm_iteration(A, b, l, n):
+    if(np.isscalar(A)):
+        return np.divide(b, A) + (1.0 / l)
+    return np.linalg.inv(A.T @ A + l * np.eye(n)) @ A.T @ b
+
+def gauss_newton(f, Df, x1, k_max = 100, tol = 1e-6):
     xk = x1
     for _ in range(k_max):
-        fxk = f(xk)
-        #print("printei!")
-        #print(fxk)
-        # Condição de terminação precoce (VMLS, p. 387).
-        if(np.linalg.norm(fxk) <= err):
+        if(np.linalg.norm(f(xk)) < tol):
             break
-        safe_df = safe_matrix(Df(xk))
-        # Condição de terminação precoce (ld em Df).
-        inds = sympy.Matrix(safe_df).rref()[1]
-        if(safe_df.shape[1] != len(inds)):
-            raise ValueError("As colunas de Df(xk) não são linearmente independentes.")
-        xk = xk - safe_lstsq(Df(xk), fxk)
+        # Iteração do método de Gauss-Newton
+        # Fórmula (18.6) do VMLS
+        xk -= lstsq(Df(xk), f(xk))
     return xk
 
-def newton(f, Df, x1, k_max = 10, err = 1e-6):
+def newton(f, Df, x1, k_max = 100, tol = 1e-6):
     xk = x1
     for _ in range(k_max):
-        fxk = f(xk)
-        if(np.linalg.norm(fxk) <= err):
+        if(np.linalg.norm(f(xk)) < tol):
             break
-        safe_df = safe_matrix(Df(xk))
-        inds = sympy.Matrix(safe_df).rref()[1]
-        if(safe_df.shape[1] != len(inds)):
-            raise ValueError("As colunas de Df(xk) não são linearmente independentes.")
-        xk = xk - safe_solve(fxk, Df(xk))
-    return xk
+        # Iteração do método de Newton
+        # Desenvolvimento da (18.6), pág. 388 do VMLS.
+        # Basicamente, como sabe-se que f: R^n -> R^n, pode-se
+        # apenas resolver um sistema linear.
+        xk -= solve(Df(xk), f(xk))
+    return xk  
 
-'''def levenberg_marquardt(f, Df, x1, lambda1, k_max = 100, err = 1e-6):
+def levenberg_marquardt(f, Df, x1, lambda1, k_max = 100, tol = 1e-6):
     xk = x1
-    l = lambda1 # lambda é palavra reservada em Python
-    N = len(x1)
+    x_shape = None if np.isscalar(x1) else x1.shape[0]
+    l = lambda1 # variável l pois lambda é uma palavra reservada em Python
     for _ in range(k_max):
-        fxk = f(xk)
-        if(np.linalg.norm(fxk) <= err):
+        # pág. 393 VMLS
+        # Stopping criteria - Small residual
+        if(np.linalg.norm(f(xk)) < tol):
             break
-        Dfxk = Df(xk)
-        # Passos 1 e 2 do VMLS
-        # (minimização de x_(k+1) em função do termo apresentado, usando mínimos quadrados linear)
-        # Linha baseada na solução do Python Companion: https://github.com/vbartle/VMLS-Companions
-        xkp1 = xk - np.linalg.lstsq(np.vstack([Dfxk, np.sqrt(l) * np.eye(N)]), np.vstack([fxk, np.zeros((N, 1))]), rcond = None)[0]
-        # Passo 3 do VMLS
-        if(np.linalg.norm(f(xkp1)) < np.linalg.norm(fxk)):
+        # Stopping criteria - Small optimality condition residual
+        if(np.linalg.norm(lm_op_res(Df(xk), f(xk))) < tol):
+            break
+        x_next = xk - lm_iteration(Df(xk), f(xk), l, x_shape)
+        # Passo 3 do pseudo-algoritmo do VMLS
+        # Definindo lambda e o valor de x
+        if(np.linalg.norm(f(x_next)) < np.linalg.norm(f(xk))):
             l *= 0.8
-            # Atualiza o x
-            xk = xkp1
-            continue
-        # Não atualiza o x
-        l *= 2.0
-    return xk'''
-
-def levenberg_marquardt(f, Df, x1, lambda1, kmax = 100, tol = 1e-6):
-    n = len(x1)
-    x = x1
-    lambd = lambda1
-    objectives = [] 
-    residuals = []
-    for k in range(kmax):
-        fk = f(x)
-        Dfk = Df(x)
-        objectives.append(npl.norm(fk)**2)
-        residuals.append(npl.norm(2*np.matmul(Dfk.T,fk)))
-        if npl.norm(2*np.matmul(Dfk.T,fk)) < tol:
-            break
-        xt = x - npl.lstsq(np.vstack([Dfk,np.sqrt(lambd)*np.eye(n)]),np.vstack([fk,np.zeros((n,1))]))[0]
-        if npl.norm(f(xt)) < npl.norm(fk):
-            lambd = .8*lambd
-            x = xt
+            xk = x_next
         else:
-            lambd = 2.0*lambd
-    return x, dict([("objectives", objectives),("residuals",residuals)])
-
+            l *= 2.0
+    return xk
--- src/nls/teste.py
+++ src/nls/teste.py
@@ -1,12 +1,14 @@
 import numpy as np
 from nls import gauss_newton, newton, levenberg_marquardt
 
-# Testes baseados no VMLS Python Companion
-# https://github.com/vbartle/VMLS-Companions
+# Teste do exemplo (18.10) do VMLS
+# A função f converge muito rapidamente para 0
+# Logo, espera-se que o resultado obtido seja muito próximo de 0
 
 f = lambda x: (np.exp(x) - np.exp(-x))/(np.exp(x)+np.exp(-x))
-Df = lambda x: 4 / (np.exp(x) + np.exp(-x))**2
+# Derivada de f
+Df = lambda x: (4 * np.exp(2 * x)) / ((np.exp(2 * x) + 1)**2)
 
 print(newton(f, Df, 0.95, 20, 1e-6))
 print(gauss_newton(f, Df, 0.95, 20, 1e-6))
-print(levenberg_marquardt(f, Df, np.array([0.95]), 1.0, 20, 1e-6)[0])
+print(levenberg_marquardt(f, Df, 0.95, 1.0, 20, 1e-6))
