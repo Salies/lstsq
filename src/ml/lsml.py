0e8a10d34740a98b924844b57973c5279d7ed7be
Daniel Serezane <serezane@protonmail.com>
2022-07-08 09:36:20 -0300

    ML padronizado (Fujii)

--- src/ml/lsml.py
+++ src/ml/lsml.py
@@ -2,14 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import load_iris
 
-# Erro soma dos quadrados, de Fuiji
-def lse(predictions, targets):
-    return sum(((predictions - targets) ** 2)) / 2
+# Erro soma dos quadrados, de Fujii
+def lse(fx, y):
+    return sum(((y - fx) ** 2)) / 2
 
 # Função de predição
-# afim: f(x) = w * x + b
-def f(x, w, b):
-    return (w * x) + b
+# afim: f(x) = a * x + b
+def f(x, a, b):
+    return (a * x) + b
 
 # Preparando o dataset
 iris = np.array(load_iris()['data'])
@@ -20,10 +20,10 @@ y = iris[:, 2]
 
 # Como a convergência independe de w e b
 # definem-se arbitrariamente como 0
-w = 0.0
+a = 0.0
 b = 0.0
 
-# Calculando a matriz A como em (Fuiji)
+# Calculando a matriz A como em (Fujii)
 z = np.sum(x ** 2)
 o = np.sum(x)
 n = len(x)
@@ -40,24 +40,21 @@ eig = np.linalg.eigvals(A)
 # pego o "meio termo"
 learning_rate = 1.0 / np.max(eig)
 
-print(f"Estado inicial:\nerro acumulado: {lse(f(x, w, b), y)}\nw: {w}, b: {b}")
+print(f"Estado inicial:\nerro acumulado: {lse(f(x, a, b), y)}\na: {a}, b: {b}")
 
 for i in range(10000):
-    prediction_err = f(x, w, b) - y
-    acc_grad_w0 = np.sum(prediction_err * x)
-    acc_grad_b = np.sum(prediction_err)
+    # Atualizando por gradiente descendente, de acordo com Fujii
+    prediction_err = y - f(x, a, b)
+    a += learning_rate * np.sum(prediction_err * x)
+    b += learning_rate * np.sum(prediction_err)
 
-    # Atualiza o peso e o bias
-    w -= learning_rate * acc_grad_w0
-    b -= learning_rate * acc_grad_b
-
-    # Relata a cada 1000 iterações
-    if i % 1000 == 0:
-        print(f"\nIteração {i}:\nerro acumulado: {lse(f(x, w, b), y)}\nw: {w}, b: {b}")
+    # Relata a cada 100 iterações
+    if i % 100 == 0:
+        print(f"\nIteração {i}:\nerro acumulado: {lse(f(x, a, b), y)}\na: {a}, b: {b}")
             
-print(f"\nEstado final:\nerro acumulado: {lse(f(x, w, b), y)}\nw: {w}, b: {b}")
+print(f"\nEstado final:\nerro acumulado: {lse(f(x, a, b), y)}\na: {a}, b: {b}")
 
 # A partir da Sepal Length, tentar prever a Petal Length
 plt.scatter(iris[:, 0], iris[:, 2], color='none', edgecolors='violet')
-plt.plot(iris[:, 0], w * iris[:, 0] + b, color='red')
+plt.plot(iris[:, 0], a * iris[:, 0] + b, color='red')
 plt.show()
